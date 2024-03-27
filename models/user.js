0e88476dae4f74c5416c7513c5a9bac7ed2fa9eb
Kenny Nguyen <sokenns@gmail.com>
2024-03-27 13:11:41 -0700

    Add min length to name fields

--- models/user.js
+++ models/user.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  first_name: { type: String, required: true, maxLength: 30 },
-  last_name: { type: String, required: true, maxLength: 30 },
-  username: { type: String, required: true, maxLength: 30 },
+  first_name: { type: String, required: true, minLength: 1, maxLength: 30 },
+  last_name: { type: String, required: true, minLength: 1, maxLength: 30 },
+  username: { type: String, required: true, minLength: 1, maxLength: 30 },
   password: { type: String, required: true, minLength: 10 },
   membership: { type: Boolean, require: true },
   admin: { type: Boolean, require: true },
