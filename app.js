58667b4ebf6964ac855a112e77247a70c54dfd5a
Kenny Nguyen <sokenns@gmail.com>
2024-03-26 17:43:29 -0700

    Remove unused user router

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
 
 var app = express();
 
@@ -44,7 +43,6 @@ app.use(
 );
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
