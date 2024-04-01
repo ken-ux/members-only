6b461044ac3ac3e2cd1b661a59579feb62ba9438
Kenny Nguyen <sokenns@gmail.com>
2024-04-01 13:10:45 -0700

    Configure font family in Tailwind settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./views/*.pug"],
   theme: {
+    fontFamily: {
+      sans: ["Lucida Grande", "Helvetica", "Arial", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
